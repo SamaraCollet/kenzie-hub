0d858f1d48a2ad27c8f7cb2d69d494060f4f0f7e
Bernardo Marques <bernardohmarques@gmail.com>
2020-12-08 11:39:22 -0300

    Add Routes to app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import Header from "./components/header";
+import { Switch, Route } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      Olá
-    </div>
+    <>
+      <Switch>
+        <Route exact path="/login">
+          {/* Page do login */}
+        </Route>
+        <Route exact path="/register">
+          {/* Page do register */}
+        </Route>
+        <Route path="/">{/* Page da home */}</Route>
+      </Switch>
+      <div>Olá</div>
+    </>
   );
 }
 
