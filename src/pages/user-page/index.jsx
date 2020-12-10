34298269a7a10b7e465bd3baeb7f3c8f30550897
Bernardo Marques <bernardohmarques@gmail.com>
2020-12-10 16:47:27 -0300

    fix: Tabs not showing correctly

--- src/pages/user-page/index.jsx
+++ src/pages/user-page/index.jsx
@@ -32,7 +32,7 @@ const UserPage = () => {
             onChange={handleChange}
             indicatorColor="primary"
             textColor="primary"
-            centered
+            scrollButtons="auto"
           >
             <Tab label="Sobre" />
             <Tab label="Experiências " />
@@ -48,13 +48,7 @@ const UserPage = () => {
             <TabContent value={value} index={0}>
               O profissional tem experiência nas tecnologias JAVASCRIPT, CSS,
               HTML, e conhecimento nos frameworks: Angular, Css, Html, .NET
-              FRAMEWORK, Spring. Ele está ativo no mercado de trabalho há
-              aproximadamente 6 anos. Ele já recebeu de pessoas do seu convivio
-              profissional 2 recomendações. O desenvolvedor tem interesse nos
-              seguintes assuntos: Análise de Dados, Blockchain e Registros de
-              Distribuídos. Já trabalhou nas empresas Vulpi, Squadra Tecnologia,
-              APTA Sistemas de Automação e possui 12 projetos publicados no
-              GitHub.
+              FRAMEWORK, Spring.
             </TabContent>
             <TabContent value={value} index={1} dir={theme.direction}>
               AEAOEKAOSKDAK
