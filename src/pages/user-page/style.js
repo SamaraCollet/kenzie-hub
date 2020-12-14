7f746e7bb862dde9fe806f0b2ebdf67b0f4655e2
Carlos Santos <carlosrsantos2001@gmail.com>
2020-12-14 14:37:06 -0300

    [feat] new style for user infos

--- src/pages/user-page/style.js
+++ src/pages/user-page/style.js
@@ -8,4 +8,17 @@ export const UserContainer = styled.div`
 
 export const TabContainer = styled.div`
   min-height: 70vh;
+  max-height: 70vh;
+`;
+
+export const ContainerBio = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin-top: 1rem;
+  img {
+    border-radius: 50%;
+  }
+  .btnSalvar {
+    margin-top: 1rem;
+  }
 `;
