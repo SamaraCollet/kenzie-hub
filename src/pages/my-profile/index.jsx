e5885553f9e54d51894ee22029ddaef8bf34a936
Carlos Santos <carlosrsantos2001@gmail.com>
2020-12-15 18:00:54 -0300

    organizando código do index da pag de myprofile

--- src/pages/my-profile/index.jsx
+++ src/pages/my-profile/index.jsx
@@ -1,36 +1,42 @@
-import Container from "@material-ui/core/Container";
-import Paper from "@material-ui/core/Paper";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import { useTheme } from "@material-ui/core/styles";
-import SwipeableViews from "react-swipeable-views";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-import ListItemText from "@material-ui/core/ListItemText";
-import TabContent from "./tab-content";
-import IconButton from "@material-ui/core/IconButton";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import Avatar from "@material-ui/core/Avatar";
-import WorkIcon from "@material-ui/icons/Work";
-import DeleteIcon from "@material-ui/icons/Delete";
-import CloseIcon from "@material-ui/icons/Close";
-import { useParams } from "react-router-dom";
-import FormControl from "@material-ui/core/FormControl";
-import InputLabel from "@material-ui/core/InputLabel";
-import CreateIcon from "@material-ui/icons/Create";
-import CheckIcon from "@material-ui/icons/Check";
-import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
-import Select from "@material-ui/core/Select";
-import MenuItem from "@material-ui/core/MenuItem";
 import { useState, useEffect } from "react";
-import { UserContainer, ContainerBio } from "./style";
-import axios from "axios";
-import { useHistory } from 'react-router-dom'
+import { useParams, useHistory } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { UserContainer, ContainerBio } from "./style";
+import TabContent from "./tab-content";
+import axios from "axios";
+
+import {
+  Container,
+  Paper,
+  Tabs,
+  Tab,
+  TextField,
+  Button,
+  SwipeableViews,
+  CircularProgress,
+  List,
+  ListItem,
+  ListItemSecondaryAction,
+  ListItemText,
+  IconButton,
+  ListItemAvatar,
+  Avatar,
+  FormControl,
+  InputLabel,
+  CreateIcon,
+  CheckIcon,
+  Select,
+  MenuItem,
+} from "@material-ui/core";
+
+import {
+  WorkIcon,
+  DeleteIcon,
+  CloseIcon,
+  AddCircleOutlineIcon,
+} from "@material-ui/icons";
+
+import { useTheme } from "@material-ui/core/styles";
 
 const MyProfile = () => {
   const [change, setChange] = useState(false);
@@ -39,12 +45,12 @@ const MyProfile = () => {
   const [currentUser, setUser] = useState({});
   const theme = useTheme();
   const [value, setValue] = useState(0);
-  const { id } = useParams()
-  const userToken = useSelector(state => state.currentUserToken)
-  const userID = window.localStorage.getItem('userID')
-  const history = useHistory()
+  const { id } = useParams();
+  const userToken = useSelector((state) => state.currentUserToken);
+  const userID = window.localStorage.getItem("userID");
+  const history = useHistory();
 
-  !userToken && history.push('/notauthorized')
+  !userToken && history.push("/notauthorized");
 
   const handleChange = (evt, newValue) => {
     setValue(newValue);
