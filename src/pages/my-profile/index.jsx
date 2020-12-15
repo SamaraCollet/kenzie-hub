630932d9f3c900871f0c6ebe5a315e2d4a1b56db
Carlos Santos <carlosrsantos2001@gmail.com>
2020-12-15 10:53:10 -0300

    add add button, change user infos

--- src/pages/my-profile/index.jsx
+++ src/pages/my-profile/index.jsx
@@ -17,19 +17,20 @@ import ListItemAvatar from "@material-ui/core/ListItemAvatar";
 import Avatar from "@material-ui/core/Avatar";
 import WorkIcon from "@material-ui/icons/Work";
 import DeleteIcon from "@material-ui/icons/Delete";
-import { useParams } from 'react-router-dom'
-import CreateIcon from '@material-ui/icons/Create';
+import { useParams } from "react-router-dom";
+import CreateIcon from "@material-ui/icons/Create";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 import { useState, useEffect } from "react";
 import { UserContainer, ContainerBio } from "./style";
 import axios from "axios";
 
 const MyProfile = () => {
-  const [isEditable, setEditable] = useState(false)
+  const [isEditable, setEditable] = useState(false);
   const [currentUser, setUser] = useState({});
   const theme = useTheme();
   const [value, setValue] = useState(0);
-  const { id } = useParams()
-  const userID = window.localStorage.getItem('userID')
+  const { id } = useParams();
+  const userID = window.localStorage.getItem("userID");
 
   const handleChange = (evt, newValue) => {
     setValue(newValue);
@@ -52,95 +53,117 @@ const MyProfile = () => {
       .catch((err) => console.log(err));
   };
 
-
   return (
     <UserContainer>
-        {isEditable ? (
-        
-            <div>Oi</div>
-        ) : (
-            <Container maxWidth="md">
-                <Paper>
-                <Tabs
-                value={value}
-                onChange={handleChange}
-                indicatorColor="primary"
-                textColor="primary"
-                scrollButtons="auto"
-              >
-                <Tab label="Bio" />
-                <Tab label="Tecnologias" />
-                <Tab label="Trabalhos" />
-                <Tab label="Curso" />
-              </Tabs>
-              <SwipeableViews
-                axis={theme.direction === "rtl" ? "x-reverse" : "x"}
-                index={value}
-                onChangeIndex={handleChangeIndex}
-              >
-
-                <TabContent value={value} index={0}>
-                  <ContainerBio>
-                    {!currentUser.avatar_url ? (
-                      <CircularProgress />
-                    ) : (
-                      <img src={currentUser.avatar_url} alt="img profile" />
-                    )}
-                    <div>Nome: <span>{currentUser.name}</span></div>
-                    <div>Bio: <span>{currentUser.bio}</span></div>
-                    
-                    <IconButton onClick={() => setEditable(true)}><CreateIcon/></IconButton>
-                  </ContainerBio>
-                </TabContent>
-
-                <TabContent value={value} index={1} dir={theme.direction}>
+      {isEditable ? (
+        <div>Oi</div>
+      ) : (
+        <Container maxWidth="md">
+          <Paper>
+            <Tabs
+              value={value}
+              onChange={handleChange}
+              indicatorColor="primary"
+              textColor="primary"
+              scrollButtons="auto"
+            >
+              <Tab label="Bio" />
+              <Tab label="Tecnologias" />
+              <Tab label="Trabalhos" />
+              <Tab label="Curso" />
+            </Tabs>
+            <SwipeableViews
+              axis={theme.direction === "rtl" ? "x-reverse" : "x"}
+              index={value}
+              onChangeIndex={handleChangeIndex}
+            >
+              <TabContent value={value} index={0}>
+                <ContainerBio>
+                  {!currentUser.avatar_url ? (
+                    <CircularProgress />
+                  ) : (
+                    <img
+                      src={currentUser.avatar_url}
+                      alt="img profile"
+                      width="200"
+                    />
+                  )}
                   <List>
-                    {currentUser.techs ? (
-                      currentUser.techs.map((tech, index) => (
-                        <ListItem key={index}>
-                          <ListItemText
-                            primary={tech.title}
-                            secondary={tech.status}
-                          />
-                        </ListItem>
-                      ))
-                    ) : (
-                      <div>Carregando...</div>
-                    )}
-                    <IconButton onClick={() => setEditable(true)}><CreateIcon/></IconButton>
+                    <ListItem>
+                      <ListItemText
+                        primary={currentUser.name}
+                        secondary={currentUser.bio}
+                      />
+                    </ListItem>
+                    <ListItemSecondaryAction>
+                      <IconButton onClick={() => setEditable(true)}>
+                        <CreateIcon />
+                      </IconButton>
+                    </ListItemSecondaryAction>
                   </List>
-                </TabContent>
+                </ContainerBio>
+              </TabContent>
 
-                <TabContent value={value} index={2} dir={theme.direction}>
-                  <List>
-                    {currentUser.works ? (
-                      currentUser.works.map((work, index) => (
-                        <ListItem key={index}>
-                          <ListItemAvatar>
-                            <Avatar>
-                              <WorkIcon />
-                            </Avatar>
-                          </ListItemAvatar>
-                          <ListItemText
-                            primary={work.title}
-                            secondary={work.description}
-                          />
-                        </ListItem>
-                      ))
-                    ) : (
-                      <div>Carregando...</div>
-                    )}
-                    <IconButton onClick={() => setEditable(true)}><CreateIcon/></IconButton>
-                  </List>
-                </TabContent>
-                <TabContent value={value} index={3} dir={theme.direction}>
-                  {currentUser.course_module}
-                  <IconButton onClick={() => setEditable(true)}><CreateIcon/></IconButton>
-                </TabContent>
-              </SwipeableViews>
-            </Paper>
-            </Container>
-        )}
+              <TabContent value={value} index={1} dir={theme.direction}>
+                <List>
+                  {currentUser.techs ? (
+                    currentUser.techs.map((tech, index) => (
+                      <ListItem key={index}>
+                        <ListItemText
+                          primary={tech.title}
+                          secondary={tech.status}
+                        />
+                      </ListItem>
+                    ))
+                  ) : (
+                    <div>Carregando...</div>
+                  )}
+                  <IconButton>
+                    <AddCircleOutlineIcon />
+                  </IconButton>
+                  <IconButton onClick={() => setEditable(true)}>
+                    <CreateIcon />
+                  </IconButton>
+                </List>
+              </TabContent>
+
+              <TabContent value={value} index={2} dir={theme.direction}>
+                <List>
+                  {currentUser.works ? (
+                    currentUser.works.map((work, index) => (
+                      <ListItem key={index}>
+                        <ListItemAvatar>
+                          <Avatar>
+                            <WorkIcon />
+                          </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText
+                          primary={work.title}
+                          secondary={work.description}
+                        />
+                      </ListItem>
+                    ))
+                  ) : (
+                    <div>Carregando...</div>
+                  )}
+                  <IconButton>
+                    <AddCircleOutlineIcon />
+                  </IconButton>
+                  <IconButton onClick={() => setEditable(true)}>
+                    <CreateIcon />
+                  </IconButton>
+                </List>
+              </TabContent>
+              <TabContent value={value} index={3} dir={theme.direction}>
+                {currentUser.course_module}
+                <IconButton onClick={() => setEditable(true)}>
+                  <CreateIcon />
+                </IconButton>
+              </TabContent>
+            </SwipeableViews>
+          </Paper>
+        </Container>
+      )}
     </UserContainer>
   );
 };
