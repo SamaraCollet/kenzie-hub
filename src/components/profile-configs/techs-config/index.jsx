864c04e082ab02961803a1f2e1494194692ad545
Bernardo Marques <bernardohmarques@gmail.com>
2020-12-16 14:36:31 -0300

    can now add new tech to profile

--- src/components/profile-configs/techs-config/index.jsx
+++ src/components/profile-configs/techs-config/index.jsx
@@ -3,74 +3,74 @@ import {
   List,
   ListItem,
   ListItemText,
-  Modal,
-  Backdrop,
-  Fade,
   TextField,
+  Select,
+  MenuItem,
+  FormControl,
   Button,
 } from "@material-ui/core";
+import AddIcon from "@material-ui/icons/Add";
 import { useSelector } from "react-redux";
 import axios from "axios";
-import { ModalStyle } from "./style";
+import { Content } from "./style";
 
 const TechConfig = () => {
-  const [currentUser, setUser] = useState({});
-  const [open, setOpen] = useState(false);
-
+  const [techInput, setInput] = useState("");
+  const [level, setLevel] = useState("Básico");
   const userInfos = useSelector((state) => state.currentUserToken);
   const userID = userInfos.user?.id;
 
-  const profileRequest = (id) => {
-    axios
-      .get(`https://kenziehub.me/users/${id}`)
-      .then((res) => {
-        setUser(res.data);
-      })
-      .catch((err) => console.log(err));
+  const handleLevel = (evt) => {
+    setLevel(evt.target.value);
   };
 
-  const modalToggle = () => {
-    setOpen(!open);
+  const handleInput = (evt) => {
+    setInput(evt.target.value);
   };
 
-  useEffect(() => {
-    if (userID) {
-      profileRequest(userID);
-    }
-    return;
-  }, []);
+  console.log(userInfos);
+
+  const handleTech = (evt) => {
+    evt.preventDefault();
+    axios({
+      method: "post",
+      url: `https://kenziehub.me/users/techs/`,
+      headers: {
+        Authorization: `Bearer ${userInfos.token}`,
+      },
+      data: { title: `${techInput}`, status: `${level}` },
+    }).catch((err) => console.log(err));
+  };
 
   return (
     <>
-      <div>
-        <button type="button" onClick={modalToggle}>
-          Adicionar Tecnologia
-        </button>
-        <Modal
-          open={open}
-          onClose={modalToggle}
-          BackdropComponent={Backdrop}
-          Backdrop={{ timeout: 500 }}
-        >
-          <Fade in={open}>
-            <ModalStyle>
-              <div className="container">
-                <h1>Adicionar Tecnologia</h1>
-                <div>
-                  <TextField label="Nome da tecnologia" />
-                </div>
-                <div>
-                  <TextField label="Status (Iniciante...)" />
-                </div>
-                <Button>Adicionar </Button>
-              </div>
-            </ModalStyle>
-          </Fade>
-        </Modal>
-      </div>
+      <form onSubmit={handleTech}>
+        <Content>
+          <FormControl>
+            <TextField name="title" value={techInput} onChange={handleInput} />
+          </FormControl>
+          <FormControl>
+            <Select
+              label="Nível"
+              name="status"
+              value={level}
+              onChange={handleLevel}
+            >
+              <MenuItem value="Iniciante">Básico</MenuItem>
+              <MenuItem value="Intermediário">Intermediário</MenuItem>
+              <MenuItem value="Avançado">Avançado</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl>
+            <Button type="submit">
+              <AddIcon />
+            </Button>
+          </FormControl>
+        </Content>
+      </form>
       <List>
-        {currentUser.techs ? (
-          currentUser.techs.map((tech, index) => (
+        {userInfos.user.techs ? (
+          userInfos.user.techs.map((tech, index) => (
             <ListItem key={index}>
               <ListItemText primary={tech.title} secondary={tech.status} />
             </ListItem>
--- src/components/profile-configs/techs-config/style.js
+++ src/components/profile-configs/techs-config/style.js
@@ -1,12 +1,5 @@
 import styled from "styled-components";
-import { Container } from "../../../styles/styles_login_register";
 
-export const ModalStyle = styled(Container)`
-  height: 370px;
-  background: #fafafa;
-  position: absolute;
-  left: calc(50% - 15%);
-  h1 {
-    text-shadow: none;
-  }
+export const Content = styled.div`
+  width: 100%;
 `;
